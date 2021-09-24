4032cc3311f018931b2c88673366fe10d0272278
Gherraz Salah <gherrazs.salah@gmail.com>
2021-09-24 11:18:00 +0200

    Second commit

--- src/mem.c
+++ src/mem.c
@@ -7,8 +7,25 @@
 //-------------------------------------------------------------
 // mem_init
 //-------------------------------------------------------------
+static fb fb_head;
+static al al_head;
+
+static int set_bit_bloc_alloc(size_t taille_bloc){
+    return taille_bloc & 0xFFFF;
+}
+
+static int set_bit_bloc_free(size_t taille_bloc){
+    return taille_bloc & (0xFFFF >> 1);
+}
+
+static int get_bit_bloc(size_t taille_bloc){
+    return taille_bloc >> sizeof(size_t) - 1;
+}
+
 void mem_init() {
-    /* A COMPLETER */
+    mem_fit(mem_fit_first);
+    fb_head.taille_bloc_libre = set_bit_bloc_free(get_memory_size());
+    fb_head.next = (fb *)get_memory_adr() + 1;
     return;
 }
 
@@ -16,7 +33,7 @@ void mem_init() {
 // mem_alloc
 //-------------------------------------------------------------
 void *mem_alloc(size_t size) {
-    /* A COMPLETER */
+    
     return NULL;
 }
 
@@ -33,7 +50,18 @@ void mem_free(void *zone) {
 // mem_show
 //-------------------------------------------------------------
 void mem_show(void (*print)(void *, size_t, int free)) {
-    /* A COMPLETER */
+    void * tmp = fb_head.next;
+    while(tmp != NULL || tmp == get_memory_adr()){
+        if(get_bit_bloc(*(int *)tmp){
+            fb * fb_tmp = (fb *)tmp;
+            printf(tmp, fb_tmp->taille_bloc, 1);
+            tmp = tmp + sizeof(size_t);
+        } else {
+            al * al_tmp = (al *) tmp;
+            printf(tmp, al_tmp->taille_bloc, 0);
+            tmp = tmp + (size_t) tmp->taille_bloc;
+        }
+    } 
     return;
 }
 
